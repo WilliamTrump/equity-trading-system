cef84957e35e77ae9f1334e2648b49b980aff305
Max Rabinowitz <Axrabz9@gmail.com>
2026-06-26 12:33:50 -0400

    changed redis to have a max connections that is configured through os variables and made account_perms_change return the new details of that account

--- api/app/core/redis.py
+++ api/app/core/redis.py
@@ -1,15 +1,20 @@
-from redis.asyncio import Redis
-from app.core.config import redis_host, redis_port_number
+from redis.asyncio import ConnectionPool, Redis
+from app.core.config import redis_host, redis_port_number, computed_max_connections
 
-redis_client = Redis(
+# 5. Initialize your pool using the computed value
+pool = ConnectionPool(
     host=redis_host,
     port=redis_port_number,
     db=0,
+    max_connections=computed_max_connections,
+    decode_responses=True,
 )
+
+redis_client = Redis(ConnectionPool=pool)
 
 redis_dictionaries = [
     "users",
     "accounts",
     "positions",
-    "username"
+    "username",
 ]  # redis dicts TODO update these tables once agrred upon naming convention
